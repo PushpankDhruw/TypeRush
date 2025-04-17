d63968996b898cd993d27b8a6ac0e0f0ec938a4e
Pushpank Dhruw <Pushpank1010@Gmail.Com>
2025-04-18 03:21:01 +0530

    ✨ refactor: update FocusWrapper component with Tailwind CSS for improved styling and layout

--- src/components/FocusWrapper/FocusWrapper.tsx
+++ src/components/FocusWrapper/FocusWrapper.tsx
@@ -1,12 +1,12 @@
-import styles from "./styles.module.css";
+// FocusWrapper.tsx
 import { PiCursorFill } from "react-icons/pi";
 
 export default function FocusWrapper() {
   return (
-    <div className={styles.wrapper}>
-      <h1>
-        <PiCursorFill />
-        Click or start typing for focus
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-[10px] bg-[rgba(0,0,0,0.1)]">
+      <h1 className="flex items-center gap-3 text-xl text-text">
+        <PiCursorFill className="text-brand" />
+        <span>Click or start typing for focus</span>
       </h1>
     </div>
   );
